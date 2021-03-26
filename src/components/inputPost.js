5b478ee642465a8b5b9d77fc398bbb41ba5d78e0
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-03-25 23:41:39 -0400

    front end update

---
+++ src/components/inputPost.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const InputPost = () => {
+    return (
+        <form>
+            <label>
+                image uploader:
+            </label>
+            <input type="date" placeholder="image"></input>
+
+        </form>
+    )
+};
+
+export default InputPost;
